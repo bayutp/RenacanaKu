496c940e81895549066ac37384c3935de420fccb
bayutp <bayuteguhpamuji@gmail.com>
2025-10-26 20:46:47 +0700

    fix null photo profil

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1430902599"
+    serviceWorkerVersion: "271030725"
   }
 });
